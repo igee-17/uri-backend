fed9c09d7d78506c8ae19733ac1a581dcd2c1d94
Olaleye Igbagbo <89357459+igee-17@users.noreply.github.com>
2024-11-14 17:47:25 +0100

    updated cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "https://uri-frontend.vercel.app",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   });
